e9f7942ac7b018036004c4a638733d2ff4f595ab
jembie <75253705+jembie@users.noreply.github.com>
2023-03-24 22:26:09 +0100

    Cleanup and Refactor

--- page_tracker/track_site.py
+++ page_tracker/track_site.py
@@ -10,43 +10,40 @@ import time
 import logging
 import filecmp
 
-URL_TO_MONITOR : str = "https://jexam.inf.tu-dresden.de/de.jexam.web.v5/spring/welcome"
-PAYLOAD_URL = "https://discord.com/api/webhooks/1088891069783081053/E6GU7-6rp5tT-xY2vEtoNlRQtxaiUl_IXGKEKPZym4-Ad2tkFbx14P_6Q6aO4NWHlSMG"
-DELAY_TIME_SECONDS : int = 300
+URL_TO_MONITOR: str = "https://jexam.inf.tu-dresden.de/de.jexam.web.v5/spring/welcome"
+PAYLOAD_URL: str = "https://discord.com/api/webhooks/1088891069783081053/E6GU7-6rp5tT-xY2vEtoNlRQtxaiUl_IXGKEKPZym4-Ad2tkFbx14P_6Q6aO4NWHlSMG"
+DELAY_TIME_SECONDS: int = 20
 
-class Page_Tracker():
 
+class Page_Tracker:
     def content_comparison(self):
-        f1 = './previous_exam_results.txt'
-        f2 = './new_exam_results.txt'
+        f1 = "./previous_exam_results.txt"
+        f2 = "./new_exam_results.txt"
         if not os.path.exists(f2):
             return False
         return filecmp.cmp(f1, f2)
 
-
-    def filter_for_exams(self, content:str, file_name:str):
-        with open(file_name, 'w') as f:
+    def filter_for_exams(self, content: str, file_name: str):
+        with open(file_name, "w") as f:
             for line in open(content):
-                if 'INF' in line or 'PLB' in line:
+                if "INF" in line or "PLB" in line:
                     f.write(line)
 
-    def return_new_exams(File1,File2):
-        with open(File1,'r') as f:
+    def return_new_exams(self, File1, File2):
+        with open(File1, "r") as f:
             old = set(f.readlines())
 
-
-        with open(File2,'r') as f:
+        with open(File2, "r") as f:
             new = set(f.readlines())
 
         return list(new - old)
 
-
     def page_crawler(self):
 
-    ### Configurations
+        ### Configurations
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
-        options.page_load_strategy = 'none'
+        options.page_load_strategy = "none"
 
         # returns the path web driver downloaded
         chrome_path = ChromeDriverManager().install()
@@ -57,49 +54,58 @@ class Page_Tracker():
 
         driver.get(URL_TO_MONITOR)
         time.sleep(2)
-    ###
-        page_content = driver.find_element(By.CSS_SELECTOR, "div[class*='news-wrapper']")
+
+        page_content = driver.find_element(
+            By.CSS_SELECTOR, "div[class*='news-wrapper']"
+        )
 
         # check if the file exists, ran on the first iteration
         if not os.path.exists("previous_content.txt"):
-            open("previous_content.txt", 'w+').close()
+            open("previous_content.txt", "w+").close()
 
-        filehandle = open('previous_content.txt', 'w')
+        filehandle = open("previous_content.txt", "w")
         filehandle.write(page_content.text)
         filehandle.close()
 
-        self.filter_for_exams('previous_content.txt', 'previous_exam_results.txt')
+        self.filter_for_exams("previous_content.txt", "previous_exam_results.txt")
 
-        if not os.path.exists('new_exam_results.txt'):
-            open('new_exam_results.txt', 'w+').close()
+        if not os.path.exists("new_exam_results.txt"):
+            open("new_exam_results.txt", "w+").close()
 
-        if self.content_comparison() is True:
+        if self.content_comparison():
             return False
-        else:
-            with open('previous_content.txt','w') as f:
-                f.write(page_content.text)
-            self.filter_for_exams('previous_content.txt', 'previous_exam_results.txt')
-            with open('previous_exam_results.txt','r') as firstfile, open('new_exam_results.txt','a') as secondfile:
-                for line in firstfile:
-                    secondfile.write(line)
-                firstfile.close()
-                secondfile.close()
-            return True
 
+        with open("previous_content.txt", "w") as f:
+            f.write(page_content.text)
+        self.filter_for_exams("previous_content.txt", "previous_exam_results.txt")
+        with open("previous_exam_results.txt", "r") as firstfile, open(
+            "new_exam_results.txt", "a"
+        ) as secondfile:
+            for line in firstfile:
+                secondfile.write(line)
+        return True
 
     def run(self):
         log = logging.getLogger(__name__)
-        logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), format='%(asctime)s %(message)s')
+        logging.basicConfig(
+            level=os.environ.get("LOGLEVEL", "INFO"), format="%(asctime)s %(message)s"
+        )
         log.info("Running Website Monitor")
         while True:
             try:
                 if self.page_crawler():
                     log.info("Webpage has changed.")
-                    results = self.return_new_exams("./page_tracker/previous_exam_results.txt", "./page_tracker/new_exam_results.txt")
+                    results = self.return_new_exams(
+                        "./page_tracker/previous_exam_results.txt",
+                        "./page_tracker/new_exam_results.txt",
+                    )
 
-                    for index in range(0, len(results)):
+                    for result in results:
                         webhook = DiscordWebhook(url=PAYLOAD_URL)
-                        embed = DiscordEmbed(title=f' <a:bpG:890945228679299082> Prüfungserbegnis {results[index]} ist nun verfügbar.', color=2158112)
+                        embed = DiscordEmbed(
+                            title=f" <a:bpG:890945228679299082> Prüfungserbegnis {result} ist nun verfügbar.",
+                            color=2158112,
+                        )
                         webhook.add_embed(embed)
                         webhook.execute()
                 else:
@@ -107,6 +113,7 @@ class Page_Tracker():
             except:
                 log.info("Error checking website.")
             time.sleep(DELAY_TIME_SECONDS)
+
 
 if __name__ == "__main__":
     tracker = Page_Tracker()

53667e45fdd28a1abd8fbd3c1b5f2fb539b6b54e
jembie <tony_ray.marx@mailbox.tu-dresden.de>
2023-03-26 18:33:40 +0200

    Add Docstring Types for the following functions:
    - `content_comparison`
    - `return_new_exams`
    - `write_content_in_previous`
    - `page_crawler`

--- page_tracker/track_site.py
+++ page_tracker/track_site.py
@@ -17,21 +17,54 @@ DELAY_TIME_SECONDS: int = 20
 
 
 class Page_Tracker:
-    def content_comparison(self, previous, new):
+    """
+    This class contains methods to monitor the jExam.inf.tu-dresden.de web page, check if updates have happened,
+    and if so, send a notification via Discord Webhook.
+    """
+
+    def content_comparison(self, previous, new) -> bool:
+        """
+        Compares the content of two files.
+
+        Args:
+        previous (str): The path of the file with previous content of the web page.
+        new (str): The path of the file with the updated content of the web page.
+
+        Returns:
+        bool: A boolean indicating whether the two files have identical content.
+        """
+
         if not os.path.exists(new):
             return False
         return filecmp.cmp(previous, new)
 
-    def return_new_exams(self, File1, File2):
+    def return_new_exams(self, File1, File2) -> set:
+        """
+        This method filters for any new exam results.
+
+        Args:
+        File1 (str): The path of the first file.
+        File2 (str): The path of the second file.
+
+        Returns:
+        set: A set containing the intersection between the contents of two files.
+        """
         with open(File1, "r") as f:
             old = set(f.readlines())
 
         with open(File2, "r") as f:
             new = set(f.readlines())
 
-        return list(new.intersection(old))
+        return new & old
 
     def write_content_in_previous(self, content):
+        """
+        Writes the scraped content into the `previous_exams.txt`, also creates the `new_exams.txt` file if it does not already exist.
+
+        Args:
+        content (str): The content to be written to the file.
+        """
+
         if not os.path.exists("previous_exams.txt"):
             open("previous_exams.txt", "w+").close()
 
@@ -42,7 +75,15 @@ class Page_Tracker:
         if not os.path.exists("new_exams.txt"):
             open("new_exams.txt", "w+").close()
 
-    def page_crawler(self):
+    def page_crawler(self) -> bool:
+        """
+        Scrapes the webpage and checks for updates.
+            - if updates are found, it writes the content in the `new_exams.txt` file and returns `True.`
+            - otherwise it returns `False`
+
+        Returns:
+        bool: A boolean indicating whether updates were found.
+        """
 
         ### Configurations
         options = webdriver.ChromeOptions()

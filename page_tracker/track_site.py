0de608848e6b0a083e06a8fbe780feffba2c4a8f
jembie <75253705+jembie@users.noreply.github.com>
2023-03-26 17:58:36 +0200

    Remove unneeded imports

--- page_tracker/track_site.py
+++ page_tracker/track_site.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 import os

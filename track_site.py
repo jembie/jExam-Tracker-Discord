758fecb971bb1001b13a40130cad533b683dfa50
jembie <tony_ray.marx@mailbox.tu-dresden.de>
2023-03-27 13:30:41 +0200

    V1.2 - Refactor

--- track_site.py
+++ track_site.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 URL_TO_MONITOR: str = "https://jexam.inf.tu-dresden.de/de.jexam.web.v5/spring/welcome"
-PAYLOAD_URL: str = os.getenv("PAYLOARD_URL")
+PAYLOAD_URL: str = os.getenv("PAYLOAD_URL")
 DELAY_TIME_SECONDS: int = 20
 
 
@@ -34,52 +34,22 @@ class Page_Tracker:
         bool: A boolean indicating whether the two files have identical content.
         """
 
-        if not os.path.exists(new):
-            return False
         return filecmp.cmp(previous, new)
 
-    def return_new_exams(self, File1, File2) -> set:
+    def write_content_in_new(self, content):
         """
-        This method filters for any new exam results.
-
-        Args:
-        File1 (str): The path of the first file.
-        File2 (str): The path of the second file.
-
-        Returns:
-        set: A set containing the intersection between the contents of two files.
-        """
-        with open(File1, "r") as f:
-            old = set(f.readlines())
-
-        with open(File2, "r") as f:
-            new = set(f.readlines())
-
-        return new & old
-
-    def write_content_in_previous(self, content):
-        """
-        Writes the scraped content into the `previous_exams.txt`, also creates the `new_exams.txt` file if it does not already exist.
+        Writes the scraped content into`new_exams.txt`.
 
         Args:
         content (str): The content to be written to the file.
         """
 
-        if not os.path.exists("previous_exams.txt"):
-            open("previous_exams.txt", "w+").close()
-
-        filehandle = open("previous_exams.txt", "w")
-        filehandle.write(content)
-        filehandle.close()
-
-        if not os.path.exists("new_exams.txt"):
-            open("new_exams.txt", "w+").close()
+        with open("new_exams.txt", "w+") as new:
+            new.write(content)
 
     def page_crawler(self) -> bool:
         """
         Scrapes the webpage and checks for updates.
-            - if updates are found, it writes the content in the `new_exams.txt` file and returns `True.`
-            - otherwise it returns `False`
 
         Returns:
         bool: A boolean indicating whether updates were found.
@@ -101,17 +71,59 @@ class Page_Tracker:
         time.sleep(2)
 
         page_content = driver.find_element(By.TAG_NAME, "ul")
-        self.write_content_in_previous(page_content.text)
 
-        if self.content_comparison("./previous_exams.txt", "./new_exams.txt"):
-            return False
+        if not os.path.exists("previous_exams.txt"):
+            open("previous_exams.txt", "w+").close()
 
-        with open("previous_exams.txt", "r") as firstfile, open(
-            "new_exams.txt", "a"
-        ) as secondfile:
-            for line in firstfile:
-                secondfile.write(line)
-        return True
+        if not os.path.exists("new_exams.txt"):
+            with open("new_exams.txt", "w+") as new:
+                new.write(page_content.text)
+            return True
+
+        self.write_content_in_new(page_content.text)
+
+        return not self.content_comparison("previous_exams.txt", "new_exams.txt")
+
+    def return_new_exams(self, previous, new) -> set:
+        """
+        This method filters for any new exam results.
+
+        Args:
+        previous (str): The path of the first file.
+        new (str): The path of the second file.
+
+        Returns:
+        set: A set containing the difference between the contents of two files.
+        """
+        with open(previous, "r") as f:
+            old = set(f.read().split("\n"))
+
+        with open(new, "r") as f:
+            new = set(f.read().split("\n"))
+
+        return new.difference(old)  # new \ old
+
+    def send_webhook_msg(self):
+        results = self.return_new_exams(
+            "previous_exams.txt",
+            "new_exams.txt",
+        )
+        for result in results:
+            webhook = DiscordWebhook(url=PAYLOAD_URL)
+            embed = DiscordEmbed(
+                title=f" <a:bpG:890945228679299082> Prüfungsergebnis {result} ist nun verfügbar.",
+                color=2158112,
+            )
+            webhook.add_embed(embed)
+            time.sleep(1)
+            webhook.execute()
+
+    def overwrite_previous_content(self):
+        with open("new_exams.txt", "r") as new, open(
+            "previous_exams.txt", "w+"
+        ) as previous:
+            for line in new:
+                previous.write(line)
 
     def run(self):
         log = logging.getLogger(__name__)
@@ -123,20 +135,9 @@ class Page_Tracker:
             try:
                 if self.page_crawler():
                     log.info("Webpage has changed.")
-                    results = self.return_new_exams(
-                        "previous_exams.txt",
-                        "new_exams.txt",
-                    )
+                    self.send_webhook_msg()
+                    self.overwrite_previous_content()
 
-                    for result in results:
-                        webhook = DiscordWebhook(url=PAYLOAD_URL)
-                        embed = DiscordEmbed(
-                            title=f" <a:bpG:890945228679299082> Prüfungserbegnis {result} ist nun verfügbar.",
-                            color=2158112,
-                        )
-                        webhook.add_embed(embed)
-                        time.sleep(1)
-                        webhook.execute()
                 else:
                     log.info("Webpage has not changed.")
             except:

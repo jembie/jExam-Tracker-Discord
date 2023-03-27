7e45542dfd8659c2751d173cc03a37925f11c810
jembie <75253705+jembie@users.noreply.github.com>
2023-03-27 15:19:16 +0200

    Added Docstrings

--- track_site.py
+++ track_site.py
@@ -18,8 +18,30 @@ DELAY_TIME_SECONDS: int = 20
 
 class Page_Tracker:
     """
-    This class contains methods to monitor the jExam.inf.tu-dresden.de web page, check if updates have happened,
-    and if so, send a notification via Discord Webhook.
+    A class for monitoring a specific webpage and sending notifications when updates occur.
+
+    Methods:
+    --------
+    content_comparison(previous: str, new: str) -> bool:
+        Compares the content of two files.
+
+    write_content_in_new(content: str):
+        Writes the scraped content into `new_exams.txt`.
+
+    page_crawler() -> bool:
+        Scrapes the webpage and checks for updates.
+
+    return_new_exams(previous: str, new: str) -> set:
+        Filters for any new exam results.
+
+    send_webhook_msg():
+        Sends a webhook message with new exam results.
+
+    overwrite_previous_content():
+        Overwrites the content of the previous exams file with the new content.
+
+    run():
+        Runs the website monitor continuously.
     """
 
     def content_comparison(self, previous, new) -> bool:
@@ -27,11 +49,11 @@ class Page_Tracker:
         Compares the content of two files.
 
         Args:
-        previous (str): The path of the file with previous content of the web page.
-        new (str): The path of the file with the updated content of the web page.
+            previous (str): The path of the file with previous content of the web page.
+            new (str): The path of the file with the updated content of the web page.
 
         Returns:
-        bool: A boolean indicating whether the two files have identical content.
+            bool: A boolean indicating whether the two files have identical content.
         """
 
         return filecmp.cmp(previous, new)
@@ -41,7 +63,7 @@ class Page_Tracker:
         Writes the scraped content into`new_exams.txt`.
 
         Args:
-        content (str): The content to be written to the file.
+            content (str): The content to be written to the file.
         """
 
         with open("new_exams.txt", "w+") as new:
@@ -52,7 +74,7 @@ class Page_Tracker:
         Scrapes the webpage and checks for updates.
 
         Returns:
-        bool: A boolean indicating whether updates were found.
+            bool: A boolean indicating whether updates were found.
         """
 
         ### Configurations
@@ -60,7 +82,7 @@ class Page_Tracker:
         options.add_argument("--headless")
         options.page_load_strategy = "none"
 
-        # returns the path web driver downloaded
+        # path where webdriver is downlaoded
         chrome_path = "/user/bin/chromedriver"
         chrome_service = Service(chrome_path)
         # pass the defined options and service objects to initialize the web driver
@@ -89,12 +111,13 @@ class Page_Tracker:
         This method filters for any new exam results.
 
         Args:
-        previous (str): The path of the first file.
-        new (str): The path of the second file.
+            previous (str): The path of the first file.
+            new (str): The path of the second file.
 
         Returns:
-        set: A set containing the difference between the contents of two files.
+            set: A set containing the difference between the contents of two files.
         """
+
         with open(previous, "r") as f:
             old = set(f.read().split("\n"))
 
@@ -104,6 +127,10 @@ class Page_Tracker:
         return new.difference(old)  # new \ old
 
     def send_webhook_msg(self):
+        """
+        Sends a webhook message with new exam results.
+        """
+
         results = self.return_new_exams(
             "previous_exams.txt",
             "new_exams.txt",
@@ -119,6 +146,10 @@ class Page_Tracker:
             webhook.execute()
 
     def overwrite_previous_content(self):
+        """
+        Overwrites the content of the previous exams file with the new content.
+        """
+
         with open("new_exams.txt", "r") as new, open(
             "previous_exams.txt", "w+"
         ) as previous:
@@ -126,6 +157,10 @@ class Page_Tracker:
                 previous.write(line)
 
     def run(self):
+        """
+        Runs the website monitor continuously.
+        """
+
         log = logging.getLogger(__name__)
         logging.basicConfig(
             level=os.environ.get("LOGLEVEL", "INFO"), format="%(asctime)s %(message)s"
@@ -146,5 +181,6 @@ class Page_Tracker:
 
 
 if __name__ == "__main__":
+    load_dotenv()
     tracker = Page_Tracker()
     tracker.run()
